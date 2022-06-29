6aed7fe2bab9d51f8b04824e957bd47ab505ab69
Josue Alba <jozue_alba@hotmail.com>
2022-06-28 22:26:56 -0500

    Implementacion de ReactJs

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.app');
-});
 
-Route::get('home',function(){
+
+Route::get('/',function(){
     return view('welcome');
 })->middleware('auth');
